cae3bb0300e78d1500a3ef403491adef3898712d
Rusna Tan <rusna@n133-p133.eduroam.kth.se>
2020-12-09 12:35:22 +0100

    add video and snow overlay

---
+++ Magic Window/Assets/TransformScale.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformScale : MonoBehaviour
+{
+
+    private Renderer rend;
+    public GameObject testObj;
+    private Vector3 scaleChange, positionChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = testObj.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //change renderer size
+        rend.transform.localScale = new Vector3(0.05f, 0.03f, 0.03f);
+    }
+}
